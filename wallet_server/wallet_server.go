42d0a5f8bf692458272c11f057273c2eb290879d
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-12-09 13:45:03 +0900

    implement wallet server

---
+++ wallet_server/wallet_server.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+const tempDir = "wallet_server/templates"
+
+// WalletServer is a struct type for a wallet server.
+type WalletServer struct {
+	port    uint16
+	gateway string
+}
+
+// NewWalletServer creates a new wallet server.
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+
+// Port is an accesser method to wallet port.
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+// Gateway is an accessor method to wallet gateway.
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+// Index handles web request.
+func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t, _ := template.ParseFiles(path.Join(tempDir, "index.html"))
+		t.Execute(w, "")
+	default:
+		log.Printf("ERROR: Invalid HTTP Method")
+	}
+}
+
+// Run executes a wallet server.
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
+}
